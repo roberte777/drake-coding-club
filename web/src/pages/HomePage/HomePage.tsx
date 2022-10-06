65fa5bc626e8dece2fcddf423adbdd72dfc40c29
roberte777 <robewilkes@yahoo.com>
2022-10-05 22:55:29 -0500

    added some basic styling to home page

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -5,11 +5,14 @@ const HomePage = () => {
   return (
     <>
       <MetaTags title="Home" description="Home page" />
-
-      <h1>Drake's Coding Club</h1>
-      <span className="inline">
-        <p className="typewriter">Coding cool stuff</p>
-      </span>
+      <main className="px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+        <h1 className="mb-2 text-6xl font-bold tracking-tight">
+          Drake's Coding Club
+        </h1>
+        <span className="inline-block">
+          <p className="typewriter">Coding cool stuff</p>
+        </span>
+      </main>
     </>
   )
 }
